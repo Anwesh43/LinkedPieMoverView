e9340821b602e4d7e32d90647331d74c007bd333
Anwesh43 <anweshthecool0@gmail.com>
2018-08-01 00:46:28 +0530

    adding PieMoverView class

--- piemoverview/src/main/java/com/anwesh/uiprojects/piemoverview/PieMoverView.kt
+++ piemoverview/src/main/java/com/anwesh/uiprojects/piemoverview/PieMoverView.kt
@@ -3,3 +3,29 @@ package com.anwesh.uiprojects.piemoverview
 /**
  * Created by anweshmishra on 01/08/18.
  */
+
+import android.view.View
+import android.content.Context
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+
+val nodes : Int = 5
+
+class PieMoverView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
