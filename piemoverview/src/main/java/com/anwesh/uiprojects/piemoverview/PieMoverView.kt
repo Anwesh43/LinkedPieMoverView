cf1cdd0402eda1664f51b5b7d2316301d73fbfef
Anwesh43 <anweshthecool0@gmail.com>
2018-08-01 01:03:20 +0530

    adding drawPMNode canvas extension method

--- piemoverview/src/main/java/com/anwesh/uiprojects/piemoverview/PieMoverView.kt
+++ piemoverview/src/main/java/com/anwesh/uiprojects/piemoverview/PieMoverView.kt
@@ -8,11 +8,28 @@ import android.view.View
 import android.content.Context
 import android.view.MotionEvent
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.RectF
 
 val nodes : Int = 6
 
 val speed : Float = 0.025f
+
+fun Canvas.drawPMNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / nodes
+    val deg : Float = 360f / nodes
+    val size : Float = gap / 2
+    val sc1 : Float = Math.min(0.5f, scale) * 2
+    val sc2 : Float = Math.min(0.5f, Math.max(0f, scale - 0.5f)) * 2
+    paint.color = Color.parseColor("#673AB7")
+    save()
+    translate(0.05f * w + gap * i + gap / 2 + gap * sc1, h/2)
+    drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), i * deg, deg * sc2 , true, paint)
+    restore()
+}
 
 class PieMoverView(ctx : Context) : View(ctx) {
 
